2888e907bbe48b51526240d8387b4bd297cc5c24
Jboy422 <gitonly543@gmail.com>
2024-07-13 13:33:41 +0900

    Initial Commit

---
+++ SwiftDataTutorial/SwiftDataTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataTutorialApp.swift
+//  SwiftDataTutorial
+//
+//  Created by Jboy422 on 2024/07/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
